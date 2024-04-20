4c9683abc9d01b9ef50e0dcfd53c30fb0db37d79
kreema1602 <100921862+kreema1602@users.noreply.github.com>
2024-04-20 22:59:13 +0700

    LoginPageUI

--- src/DAO/test.java
+++ src/DAO/test.java
@@ -1,3 +1,5 @@
-public class Test {
-    
+package dao;
+
+public class test {
+
 }
--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
     
 }
