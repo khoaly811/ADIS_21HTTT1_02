264eebab921afb3dfdbf86b5f28640735d69317e
Trung Tín Võ <votrungtin@Macbook-cua-Tin.local>
2024-04-21 20:48:07 +0700

    Fix bug

--- src/adis02/App.java
+++ src/adis02/App.java
@@ -1,13 +1,31 @@
 package adis02;
 
-public class App {
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+
+public class App extends javafx.application.Application {
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(loadFXML("../views/loginPage"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
-        // Database connection details
-        MainDatabase db = new MainDatabase();
-        try {
-            db.connect();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        launch();
     }
 }
