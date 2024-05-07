b697395f7587c19034feffc9b8da905add197e9a
Trung Tín Võ <votrungtin@Macbook-cua-Tin.local>
2024-05-07 23:29:08 +0700

    Fix bug

--- src/adis02/App.java
+++ src/adis02/App.java
@@ -3,9 +3,12 @@ package adis02;
 import java.io.IOException;
 import DataAccessLayer.DataConnection;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends javafx.application.Application {
     private static Scene scene;
@@ -20,7 +23,10 @@ public class App extends javafx.application.Application {
     public void start(Stage stage) throws Exception {
         scene = new Scene(loadFXML("../views/loginPage"));
         stage.setScene(scene);
-        stage.setMaximized(true);
+        // stage.setMaximized(true);
+    
+        stage.setMinHeight(400);
+        stage.setMinWidth(600);
         stage.show();
     }
 
